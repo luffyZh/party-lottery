1ea1097a1a141c5b4e5f9deb89f503f973c62159
zhoudeyou <zhoudeyou@cnaeit.com>
2024-01-22 09:52:02 +0800

    :tada: feat: 增加随机转换

--- js/star-bg.js
+++ js/star-bg.js
@@ -54,7 +54,7 @@ var Star = function () {
   this.orbitX = w / 2;
   this.orbitY = h / 2;
   this.timePassed = random(0, maxStars);
-  this.speed = random(this.orbitRadius) / 50000;
+  this.speed = random(this.orbitRadius) / 100000;
   //星星移动速度
   this.alpha = random(2, 10) / 30;
 
